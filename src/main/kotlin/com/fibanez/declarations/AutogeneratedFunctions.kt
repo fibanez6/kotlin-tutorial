9ffcdf1cdd0bbd26ac26e927fd6624800aeb61c9
fibanez6 <fibanez@fibanez.com>
2018-03-10 17:01:27 +0000

    Destructuring declaration and Data class

---
+++ src/main/kotlin/com/fibanez/declarations/AutogeneratedFunctions.kt
@@ -0,0 +1,26 @@
+package com.fibanez.declarations
+
+/**
+ * Data class gets next functions, generated automatically:
+ * component functions, toString(), equals(), hashCode() and copy().
+ * See http://kotlinlang.org/docs/reference/data-classes.html#data-classes
+ */
+
+data class User1(val name: String, val id: Int)
+
+fun main(args: Array<String>) {
+    val user = User1("Alex", 1)
+    println(user)  // toString()
+
+    val secondUser = User1("Alex", 1)
+    val thirdUser = User1("Max", 2)
+
+    println("user == secondUser: ${user == secondUser}")
+    println("user == thirdUser: ${user == thirdUser}")
+
+    // copy() function
+    println(user.copy())
+    println(user.copy("Max"))
+    println(user.copy(id = 2))
+    println(user.copy("Max", 2))
+}
--- src/main/kotlin/com/fibanez/helloworld/ObjectOriented.kt
+++ src/main/kotlin/com/fibanez/helloworld/ObjectOriented.kt
@@ -1,5 +1,11 @@
 package com.fibanez.helloworld
 
+/**
+ * Here we have a class with a primary constructor and a member function.
+ * Note that there's no `new` keyword used to create an object.
+ * See http://kotlinlang.org/docs/reference/classes.html#classes
+ */
+
 class Greeter(val name: String) {
     fun greet() {
         println("Hello, ${name}")
