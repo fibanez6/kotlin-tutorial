28efd23eb1e1743259148533354fb52ee4ad0c36
fibanez6 <fibanez@fibanez.com>
2018-03-10 17:51:09 +0000

    Delegated properties

---
+++ src/main/kotlin/com/fibanez/delegatedProperties/ObservableProperty.kt
@@ -0,0 +1,22 @@
+package com.fibanez.delegatedProperties
+
+import kotlin.properties.Delegates
+
+/**
+ * The observable() function takes two arguments: initial value and a handler for modifications.
+ * The handler gets called every time we assign to `name`, it has three parameters:
+ * a property being assigned to, the old value and the new one. If you want to be able to veto
+ * the assignment, use vetoable() instead of observable().
+ */
+
+class User {
+    var name: String by Delegates.observable("no name") {
+        d, old, new ->
+        println("$old - $new")
+    }
+}
+
+fun main(args: Array<String>) {
+    val user = User()
+    user.name = "Carl"
+}
